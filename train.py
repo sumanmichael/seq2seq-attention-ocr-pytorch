cbc64fcb9ebf5f3402661cfad8ffcda6a3efec1a
Suman Michael <sumanmichael01@gmail.com>
2021-10-18 00:11:46 +0530

    replaced neptune with wandb

--- train.py
+++ train.py
@@ -2,9 +2,13 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from pytorch_lightning.loggers import WandbLogger
+
+from neptune.new.integrations.pytorch_lightning import NeptuneLogger
 
 from src.aocr import OCR, OCRDataModule
+
+import dotenv; dotenv.load_dotenv()
+
 
 
 @hydra.main(config_path="conf", config_name="config")
@@ -12,16 +16,19 @@ def main(cfg: DictConfig) -> None:
     dm = OCRDataModule(**cfg.data)
     model = OCR(optim_kwargs=cfg.optim, **cfg.model)
 
-    wandb_logger = WandbLogger(config=OmegaConf.to_container(cfg, resolve=True), **cfg.logger)
+    neptune_logger = NeptuneLogger(**cfg.logger)
     checkpoint_callback = ModelCheckpoint(**cfg.callbacks.checkpoint)
     early_stopping = EarlyStopping(**cfg.callbacks.early_stopping)
 
+    neptune_logger.experiment["parameters"] = OmegaConf.to_container(cfg, resolve=True)
+
     trainer_cfg = OmegaConf.to_container(cfg.trainer, resolve=True)
-    trainer_cfg["logger"] = wandb_logger
+    trainer_cfg["logger"] = neptune_logger
     trainer_cfg["callbacks"] = [checkpoint_callback, early_stopping]
     trainer = Trainer(**trainer_cfg)
     trainer.fit(model, dm)
 
+    neptune_logger.experiment.stop()
 
 
 if __name__ == "__main__":
