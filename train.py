d56180f89bab5eb809e995f28531eefbe674ec58
Suman Michael <sumanmichael01@gmail.com>
2021-10-20 13:53:30 +0530

    made callbacks and logger optional

--- train.py
+++ train.py
@@ -16,19 +16,26 @@ def main(cfg: DictConfig) -> None:
     dm = OCRDataModule(**cfg.data)
     model = OCR(optim_kwargs=cfg.optim, **cfg.model)
 
-    neptune_logger = NeptuneLogger(**cfg.logger)
-    checkpoint_callback = ModelCheckpoint(**cfg.callbacks.checkpoint)
-    early_stopping = EarlyStopping(**cfg.callbacks.early_stopping)
-
-    neptune_logger.experiment["parameters"] = OmegaConf.to_container(cfg, resolve=True)
+    callbacks = []
+    if cfg.callbacks.checkpoint:
+        callbacks.append(ModelCheckpoint(**cfg.callbacks.checkpoint))
+    if cfg.callbacks.early_stopping:
+        callbacks.append(EarlyStopping(**cfg.callbacks.early_stopping))
 
     trainer_cfg = OmegaConf.to_container(cfg.trainer, resolve=True)
-    trainer_cfg["logger"] = neptune_logger
-    trainer_cfg["callbacks"] = [checkpoint_callback, early_stopping]
+    trainer_cfg["callbacks"] = callbacks
+
+    neptune_logger = None
+    if cfg.logger:
+        neptune_logger = NeptuneLogger(**cfg.logger)
+        neptune_logger.experiment["parameters"] = OmegaConf.to_container(cfg, resolve=True)
+        trainer_cfg["logger"] = neptune_logger
+
     trainer = Trainer(**trainer_cfg)
     trainer.fit(model, dm)
 
-    neptune_logger.experiment.stop()
+    if neptune_logger:
+        neptune_logger.experiment.stop()
 
 
 if __name__ == "__main__":
